158b9bed8c574560f899bfb0f2751d8e12ecea04
Ezra Elon <elonezra1995@hotmail.com>
2023-08-20 12:00:35 +0300

    Update tts_playht.py

--- tts_playht.py
+++ tts_playht.py
@@ -7,9 +7,12 @@ import json
 AUTHORIZATION = ""
 X_USER_ID = ""
 
-def init(AUTHORIZATION, X_USER_ID):
-  AUTHORIZATION = this.AUTHORIZATION
-  X_USER_ID = this.X_USER_ID
+def init(auth, user_id):
+  global AUTHORIZATION
+  global X_USER_ID
+  AUTHORIZATION = auth
+  X_USER_ID = user_id
+
 
 def generate_transcript(contentArray, voice = "en-AU-Neural2-A", style = ""):
   if (AUTHORIZATION =="" or X_USER_ID == ""):
@@ -17,7 +20,9 @@ def generate_transcript(contentArray, voice = "en-AU-Neural2-A", style = ""):
     return null
   url = "https://play.ht/api/v1/convert"
   content = contentArray#["hello world, my name is Matthew.", "and i want to say some things to the world"]
-
+  sounds = get_sounds()
+  if(not(style in sounds.get(voice))):
+    style = ""
   payload = {
       "content": content,
       "voice": voice,
@@ -41,6 +46,7 @@ def generate_transcript(contentArray, voice = "en-AU-Neural2-A", style = ""):
 
   #print(response.text)
   return transcriptionId
+
 def generate_sound(transcriptionId):
   url = "https://play.ht/api/v1/articleStatus?transcriptionId=" + transcriptionId
 
@@ -61,12 +67,22 @@ def generate_sound(transcriptionId):
   return audio_url
 
 
-def play_sound(audio_url, file_name = "temp_audio.mp3"):
-  r = requests.get(audio_url, allow_redirects=True)
-  open(file_name, 'wb').write(r.content)
-  display(Audio('temp_audio.mp3', autoplay=True))
+def play_sound(audio_url = "", file_name = "speech_file.mp3"):
+  if(audio_url != ""):
+    r = requests.get(audio_url, allow_redirects=True)
+    open(file_name, 'wb').write(r.content)
+  display(Audio(file_name, autoplay=True))
 
-def get_sounds():
+
+def text_to_sound_file(contentArray, voice = "en-AU-Neural2-A", style = "",file_name = ""):
+  trans = generate_transcript(contentArray,voice,style)
+  url = generate_sound(trans)
+  r = requests.get(url, allow_redirects=True)
+  if(file_name == ""):
+    file_name = voice+"_file.mp3"
+  open(file_name, 'wb').write(r.content)
+
+def get_sounds(language = "all"):
   url = "https://play.ht/api/v1/getVoices"
   headers = {
       "accept": "application/json",
@@ -75,16 +91,25 @@ def get_sounds():
   }
 
   response = requests.get(url, headers=headers)
-  print(response.json())
+  if(response.status_code == 403):
+    print("your auth data are wrong or missing, use init to configure it.")
+    return 
+  # print(response.json())
   voices_list = response.json()["voices"]
 
   # Filter out the elements that have a "language" key that does not contain the substring "English"
-  filtered_list = [element for element in voices_list if "language" in element and "English" in element["language"]]
+  filtered_list = voices_list
+  if language != "all":
+    filtered_list = [element for element in voices_list if "language" in element and language in element["language"]]
   i = 0
+  models = {}
   for e in filtered_list:
-    print(i)
-    print(e["value"],e["gender"], e["voiceType"],e["sample"] )
-    print(e.get("styles", "empty"))
-    #play_sound(e["sample"], file_name = "sample.mp3")
-    print()
+    # print(i)
+    
+    models[e.get("value", "empty")] = (e.get("gender", "empty"), e.get("voiceType", "empty"),e.get("sample", "empty"),e.get("styles", "no_style"))
+    #print(e)
+    # print(e.get("styles", "empty"))
+    # #play_sound(e["sample"], file_name = "sample.mp3")
+    # print()
     i+=1
+  return models
